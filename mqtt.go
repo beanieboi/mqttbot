9e9c57ce3dc8269537eac0c34ac4666869e106f6
ben <beanie@benle.de>
2022-07-18 00:08:33 +0200

    refinements

--- mqtt.go
+++ mqtt.go
@@ -1,17 +1,20 @@
 package main
 
-import MQTT "github.com/eclipse/paho.mqtt.golang"
+import (
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"go.uber.org/zap"
+)
 
 func NewMQTTClient(host string, username string, password string) MQTT.Client {
 	opts := MQTT.NewClientOptions()
 	opts.AddBroker(host)
-	opts.SetClientID("luftdaten2mqtt")
+	opts.SetClientID("mqttbot")
 	opts.SetUsername(username)
 	opts.SetPassword(password)
 
 	client := MQTT.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
+		logger.Error("error connecting to MQTT", zap.Error(token.Error()))
 	}
 	return client
 }
